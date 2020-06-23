ca7e12f5992261c4db28ce7f1fec70ae60af81ca
tyr <tylerscheffler@protonmail.com>
2020-06-23 11:14:50 -0700

    Added basic error handling

--- tweets/views.py
+++ tweets/views.py
@@ -40,6 +40,10 @@ def tweet_create_view(request, *args, **kwargs):
         # if next_url != None and is_safe_url(next_url, ALLOWED_HOSTS):
         #     return redirect(next_url)
         form=TweetForm() #re-initialize new blank form
+    if form.errors:
+        if request.is_ajax():
+            return JsonResponse(form.errors, status=400)
+
     return render(request, 'pages/components/form.html', context={"form": form})
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
