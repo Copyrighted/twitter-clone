c2d90e326d8b59e34e670415a3f94c2f9892f8b7
tyr <tylerscheffler@protonmail.com>
2020-06-24 07:39:54 -0700

    Added searching for users associated w/ tweets in admin panel, and added user model to models.py

--- tweets/admin.py
+++ tweets/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Tweet
+
+class TweetAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'user']
+    search_fields = ['user__username', 'user__email']
+
+    # def __str__(self):
+    #     return self.content #can display content for tweets
+
+    class Meta:
+        model = Tweet
+
+admin.site.register(Tweet, TweetAdmin)
+
--- tweets/models.py
+++ tweets/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 import random
+from django.conf import settings
 # Create your models here.
 
+User = settings.AUTH_USER_MODEL #use djangos default user settings
+
 class Tweet(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE) # so many users can have many tweets, CASCADE will delete all tweets associated with user ID
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
 
